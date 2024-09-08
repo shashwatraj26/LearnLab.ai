5a551c882e994383c225def64ac738ce4f9873f4
shashwatraj26 <shashwatsahil12@gmail.com>
2024-09-09 02:24:49 +0530

    ui

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className='bg-white flex justify-between p-5 shadow-[0_10_10px_rgba(255,255,255,1)]'>
       <div className='flex'>
-      <Image src={"/Logo.png"} alt="Logo" width={50} />
+      <Image src={'/Logo.png'} width={50} height={50} />
       <h1 className="border-separate bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-serif text-transparent sm:text-2.5xl "> LearnLab.ai</h1>
       </div>
       <Button className="shadow-[0_4px_15px_rgba(255,255,255,0.3)]">Get Started</Button>
